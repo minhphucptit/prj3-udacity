3e5c71eb814700e08b32f3ca949fece0eba59b85
minhphucptit <nminhphuc99@gmail.com>
2023-09-20 00:47:47 +0700

    update screen shots

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a823596b6ccbf45a7a49b39d831c8e79-611833301.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
